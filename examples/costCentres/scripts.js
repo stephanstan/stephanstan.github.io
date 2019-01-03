2fb5f60cdfee8a72fe903d72cf72cef3801ee637
Stephan Stan <stephanstan@gmail.com>
2019-01-03 11:51:52 -0700

    Update scripts.js

--- examples/costCentres/scripts.js
+++ examples/costCentres/scripts.js
@@ -10,22 +10,24 @@ app.appendChild(logo);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+//request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+request.open('GET', 'http://omak:8070/pasdaily/rest/costCentre', true);
+
 request.onload = function () {
 
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
+    data.forEach(costCentre => {
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
 
       const h1 = document.createElement('h1');
-      h1.textContent = movie.title;
+      h1.textContent = costCentre.name;
 
       const p = document.createElement('p');
-      movie.description = movie.description.substring(0, 300);
-      p.textContent = `${movie.description}...`;
+      costCentre.code = costCentre.code.substring(0, 300);
+      p.textContent = `${costCentre.code}...`;
 
       container.appendChild(card);
       card.appendChild(h1);
