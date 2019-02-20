4963f45d534cafb647eab5bb9f2c49ee74327739
Stephan Stan <stephanstan@gmail.com>
2019-02-19 23:26:36 -0700

    path to logo_links.html

--- javascript/w3-include-html.js
+++ javascript/w3-include-html.js
@@ -1,27 +1,28 @@
-function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /*loop through a collection of all HTML elements:*/
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-        elmnt = z[i];
-        /*search for elements with a certain atrribute:*/
-        file = elmnt.getAttribute("w3-include-html");
-        if (file) {
-            /*make an HTTP request using the attribute value as the file name:*/
-            xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (this.readyState == 4) {
-                    if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-                    if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-                    /*remove the attribute, and call this function once more:*/
-                    elmnt.removeAttribute("w3-include-html");
-                    includeHTML();
+(function () {
+
+    myHTMLInclude();
+
+    function myHTMLInclude() {
+        var z, i, a, file, xhttp;
+        z = document.getElementsByTagName("*");
+        for (i = 0; i < z.length; i++) {
+            if (z[i].getAttribute("w3-include-html")) {
+                a = z[i].cloneNode(false);
+                file = z[i].getAttribute("w3-include-html");
+                var xhttp = new XMLHttpRequest();
+                xhttp.onreadystatechange = function() {
+                    if (xhttp.readyState == 4 && xhttp.status == 200) {
+                        a.removeAttribute("w3-include-html");
+                        a.innerHTML = xhttp.responseText;
+                        z[i].parentNode.replaceChild(a, z[i]);
+                        myHTMLInclude();
+                    }
                 }
+                xhttp.open("GET", file, true);
+                xhttp.send();
+                return;
             }
-            xhttp.open("GET", file, true);
-            xhttp.send();
-            /*exit the function:*/
-            return;
         }
     }
-};
+
+})();
